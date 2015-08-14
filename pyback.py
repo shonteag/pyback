e03ce86ed6895a171a49d534d01b239d2d7280ed
shonteag <shonte.amato-grill@relicananalytics.com>
2015-08-14 15:03:17 -0400

    + pub/sub static methods

---
+++ pyback.py
@@ -0,0 +1,125 @@
+"""
+Module  :: pyback
+License :: MIT
+
+A pure-python pub-sub module for registering,
+subscribing, and publishing events.
+"""
+
+# CHANNELS tracks the publisher channels.
+# 	'channel_key':[callback_function1, callback_function2, ...]
+_CHANNELS = {}
+
+class Evt(object):
+	def __init__(self, **kwargs):
+		for k, v in kwargs.items():
+			setattr(self, k, v)
+
+
+# Channel controls
+def open_channel(channel_key):
+	"""
+	Static method for creating a new channel.
+
+	If successfuly (opened, or already open),
+	return true.
+	"""
+	channel_key = str(channel_key)
+
+	if channel_key in _CHANNELS:
+		return True
+	else:
+		_CHANNELS.update({channel_key:[]})
+		return True
+
+def close_channel(channel_key):
+	"""
+	Static method for closing an entire channel.
+	This will disregard currently running pub/subs
+	and no events will be passed along this channel.
+
+	If successful, returns true.
+	"""
+	channel_key = str(channel_key)
+
+	if channel_key not in _CHANNELS:
+		return True
+	else:
+		_CHANNELS.pop(channel_key)
+		return True
+
+
+# Pub/subs
+def subscribe(channel_key, func_call):
+	"""
+	Static method for subscribing to a channel.
+
+	If the channel does not exist, it will be
+	added to _CHANNELS.
+	If it does exist, func_call will be added to
+	list of callback_functions in _CHANNELS.
+
+	func_call argument should be a method name
+	that accepts a single argument (event).
+	"""
+	channel_key = str(channel_key)
+
+	# if the subscribing to channel_key does not
+	# exist, add it, along with the function call.
+	if channel_key not in _CHANNELS:
+		_CHANNELS.update({channel_key:[func_call]})
+	else:
+		_CHANNELS[channel_key].append(func_call)
+# end subscribe
+
+def unsubscribe(channel_key, func_call):
+	"""
+	Static method to remove a func_call from
+	a channel.
+	"""
+	channel_key = str(channel_key)
+
+	if channel_key not in _CHANNELS:
+		return
+	else:
+		_CHANNELS[channel_key].remove(func_call)
+
+def publish(channel_key, **kwargs):
+	"""
+	Static method for publishing an event.
+
+	kwargs will be set as Evt object
+	attributes (setattr()), and the generated
+	Evt will be passed back to all registerd
+	subscribers.
+
+	If channel_key does not exist, it will be
+	ignored as that means there are no subscribers.
+	"""
+	channel_key = str(channel_key)
+
+	if channel_key not in _CHANNELS:
+		return
+	else:
+		# create an Evt instance
+		evt = Evt(**kwargs)
+		for func_call in _CHANNELS[channel_key]:
+			# fire each of the callbacks.
+			try:
+				func_call(evt)
+			except:
+				# func not avaible at this time.
+				pass
+# end publish
+
+
+# Instantiated
+def Publisher(object):
+	"""
+	Instantiated object for those who require it.
+	"""
+	def __init__(self, channel_key):
+		self.channel_key = str(channel_key)
+
+	def publish(**kwargs):
+		publish(self.channel_key, **kwargs)
---
+++ test_pyback.py
@@ -0,0 +1,41 @@
+import unittest
+
+class TestCase_Evt(unittest.TestCase):
+
+	def test_Evt_init(self):
+		import pyback
+		kwargs = {'testval':42}
+		evt = pyback.Evt(**kwargs)
+		self.assertEqual(evt.testval, 42)
+
+	def test_static_sub_pub(self):
+		import pyback
+		self.called = 0
+		def callback(evt):
+			self.called += 1
+			self.assertIsInstance(evt, pyback.Evt)
+			self.assertEqual(evt.testval, 42)
+
+		pyback.subscribe('testchannel', callback)
+		pyback.publish('testchannel', testval=42)
+		self.assertEqual(self.called, 1)
+
+		# remove it
+		pyback.unsubscribe('testchannel', callback)
+		pyback.publish('testchannel', testval=42)
+		self.assertEqual(self.called, 1)  # NOT 2
+
+	def test_object_callback(self):
+		import pyback
+		class Dummy(object):
+			def __init__(self):
+				self.called = 0
+
+			def object_callback(self, evt):
+				self.called += 1
+
+		dum = Dummy()
+		pyback.subscribe('testchannel', dum.object_callback)
+		pyback.publish('testchannel', thing='blah')
+		self.assertEqual(dum.called, 1)
+		pyback.unsubscribe('testchannel', dum.object_callback)
