a9c6fa35bc1953053d8de333d7d2ffa6c5934be1
shonteag <shonte.amatogrill@gmail.com>
2015-08-17 16:11:59 -0400

    pep8

--- pyback.py
+++ pyback.py
@@ -7,152 +7,156 @@ subscribing, and publishing events.
 """
 
 # CHANNELS tracks the publisher channels.
-# 	'channel_key':[callback_function1, callback_function2, ...]
+#   'channel_key':[callback_function1, callback_function2, ...]
 _CHANNELS = {}
 
 
 class PybackError(Exception):
-	pass
+    pass
+
 
 class Evt(object):
-	"""
-	Instantiates an event to be passed to
-	registered callback function(s).
+    """
+    Instantiates an event to be passed to
+    registered callback function(s).
 
-	channel is the channel_key upon which
-	the event was triggered
+    channel is the channel_key upon which
+    the event was triggered
 
-	kwargs is dict of keyword args to be
-	set as Evt attributes. It may not include
-	the keyword "channel".
-	"""
-	def __init__(self, channel, **kwargs):
-		# check to make sure there are no name
-		# conflicts.
-		if '__channel' in kwargs:
-			raise PybackError("'__channel' is reserved keyword.")
+    kwargs is dict of keyword args to be
+    set as Evt attributes. It may not include
+    the keyword "channel".
+    """
+    def __init__(self, channel, **kwargs):
+        # check to make sure there are no name
+        # conflicts.
+        if '__channel' in kwargs:
+            raise PybackError("'__channel' is reserved keyword.")
 
-		for k, v in kwargs.items():
-			self.__channel = str(channel)
-			setattr(self, k, v)
+        for k, v in kwargs.items():
+            self.__channel = str(channel)
+            setattr(self, k, v)
 
-	def get_channel(self):
-		return self.__channel
+    def get_channel(self):
+        return self.__channel
 
 
 # Channel controls
 def open_channel(channel_key):
-	"""
-	Static method for creating a new channel.
+    """
+    Static method for creating a new channel.
 
-	If successfuly (opened, or already open),
-	return true.
-	"""
-	channel_key = str(channel_key)
+    If successfuly (opened, or already open),
+    return true.
+    """
+    channel_key = str(channel_key)
 
-	if channel_key in _CHANNELS:
-		return True
-	else:
-		_CHANNELS.update({channel_key:[]})
-		return True
+    if channel_key in _CHANNELS:
+        return True
+    else:
+        _CHANNELS.update({channel_key: []})
+        return True
+
 
 def close_channel(channel_key):
-	"""
-	Static method for closing an entire channel.
-	This will disregard currently running pub/subs
-	and no events will be passed along this channel.
+    """
+    Static method for closing an entire channel.
+    This will disregard currently running pub/subs
+    and no events will be passed along this channel.
 
-	If successful, returns true.
-	"""
-	channel_key = str(channel_key)
+    If successful, returns true.
+    """
+    channel_key = str(channel_key)
 
-	if channel_key not in _CHANNELS:
-		return True
-	else:
-		_CHANNELS.pop(channel_key)
-		return True
+    if channel_key not in _CHANNELS:
+        return True
+    else:
+        _CHANNELS.pop(channel_key)
+        return True
 
 
 # Pub/subs
 def subscribe(channel_key, func_call):
-	"""
-	Static method for subscribing to a channel.
+    """
+    Static method for subscribing to a channel.
 
-	If the channel does not exist, it will be
-	added to _CHANNELS.
-	If it does exist, func_call will be added to
-	list of callback_functions in _CHANNELS.
+    If the channel does not exist, it will be
+    added to _CHANNELS.
+    If it does exist, func_call will be added to
+    list of callback_functions in _CHANNELS.
 
-	func_call argument should be a method name
-	that accepts a single argument (event).
-	If func_call is already subscribed, it will
-	NOT be subscribed twice.
-	"""
-	channel_key = str(channel_key)
+    func_call argument should be a method name
+    that accepts a single argument (event).
+    If func_call is already subscribed, it will
+    NOT be subscribed twice.
+    """
+    channel_key = str(channel_key)
 
-	# if the subscribing to channel_key does not
-	# exist, add it, along with the function call.
-	if channel_key not in _CHANNELS:
-		_CHANNELS.update({channel_key:[func_call]})
-	else:
-		# check if func_call is already subscribed
-		if func_call in _CHANNELS[channel_key]:
-			raise PybackError("{0} is already subscribed to {1}"
-				.format(func_call, channel_key))
-			return
-		else:
-			_CHANNELS[channel_key].append(func_call)
+    # if the subscribing to channel_key does not
+    # exist, add it, along with the function call.
+    if channel_key not in _CHANNELS:
+        _CHANNELS.update({channel_key: [func_call]})
+    else:
+        # check if func_call is already subscribed
+        if func_call in _CHANNELS[channel_key]:
+            raise PybackError("{0} is already subscribed to {1}"
+                              .format(func_call, channel_key))
+            return
+        else:
+            _CHANNELS[channel_key].append(func_call)
 # end subscribe
 
-def unsubscribe(channel_key, func_call):
-	"""
-	Static method to remove a func_call from
-	a channel.
-	"""
-	channel_key = str(channel_key)
 
-	if channel_key not in _CHANNELS:
-		raise 
-	else:
-		_CHANNELS[channel_key].remove(func_call)
+def unsubscribe(channel_key, func_call):
+    """
+    Static method to remove a func_call from
+    a channel.
+    """
+    channel_key = str(channel_key)
+
+    if channel_key not in _CHANNELS:
+        raise
+    else:
+        _CHANNELS[channel_key].remove(func_call)
 # end unsubscribe
 
+
 def publish(channel_key, **kwargs):
-	"""
-	Static method for publishing an event.
+    """
+    Static method for publishing an event.
 
-	kwargs will be set as Evt object
-	attributes (setattr()), and the generated
-	Evt will be passed back to all registerd
-	subscribers.
+    kwargs will be set as Evt object
+    attributes (setattr()), and the generated
+    Evt will be passed back to all registerd
+    subscribers.
 
-	If channel_key does not exist, it will be
-	ignored as that means there are no subscribers.
-	"""
-	channel_key = str(channel_key)
+    If channel_key does not exist, it will be
+    ignored as that means there are no subscribers.
+    """
+    channel_key = str(channel_key)
 
-	if channel_key not in _CHANNELS:
-		return
-	else:
-		# create an Evt instance
-		evt = Evt(channel_key, **kwargs)
-		for func_call in _CHANNELS[channel_key]:
-			# fire each of the callbacks.
-			try:
-				func_call(evt)
-			except:
-				# func not avaible at this time.
-				pass
+    if channel_key not in _CHANNELS:
+        return
+    else:
+        # create an Evt instance
+        evt = Evt(channel_key, **kwargs)
+        for func_call in _CHANNELS[channel_key]:
+            # fire each of the callbacks.
+            try:
+                func_call(evt)
+            except:
+                # func not avaible at this time.
+                pass
 # end publish
 
 
 # Instantiated
 def Publisher(object):
-	"""
-	Instantiated object for those who require it.
-	"""
-	def __init__(self, channel_key):
-		self.channel_key = str(channel_key)
+    """
+    Instantiated object for those who require it.
+    """
+    def __init__(self, channel_key):
+        self.channel_key = str(channel_key)
 
-	def publish(**kwargs):
-		publish(self.channel_key, **kwargs)
+    def publish(**kwargs):
+        publish(self.channel_key, **kwargs)
--- test_pyback.py
+++ test_pyback.py
@@ -2,96 +2,96 @@ import unittest
 
 class TestCase_Evt(unittest.TestCase):
 
-	def test_Evt_init(self):
-		import pyback
-		kwargs = {'testval':42}
-		evt = pyback.Evt('testchannel',**kwargs)
-		self.assertEqual(evt.testval, 42)
-		self.assertEqual(evt.get_channel(), 'testchannel')
+    def test_Evt_init(self):
+        import pyback
+        kwargs = {'testval':42}
+        evt = pyback.Evt('testchannel',**kwargs)
+        self.assertEqual(evt.testval, 42)
+        self.assertEqual(evt.get_channel(), 'testchannel')
 
 class TestCase_StaticMethods(unittest.TestCase):
 
-	def test_static_sub_pub(self):
-		import pyback
-		self.called = 0
-		def callback(evt):
-			self.called += 1
-			self.assertIsInstance(evt, pyback.Evt)
-			self.assertEqual(evt.testval, 42)
+    def test_static_sub_pub(self):
+        import pyback
+        self.called = 0
+        def callback(evt):
+            self.called += 1
+            self.assertIsInstance(evt, pyback.Evt)
+            self.assertEqual(evt.testval, 42)
 
-		pyback.subscribe('testchannel', callback)
-		pyback.publish('testchannel', testval=42)
-		self.assertEqual(self.called, 1)
+        pyback.subscribe('testchannel', callback)
+        pyback.publish('testchannel', testval=42)
+        self.assertEqual(self.called, 1)
 
-		# remove it
-		pyback.unsubscribe('testchannel', callback)
-		pyback.publish('testchannel', testval=42)
-		self.assertEqual(self.called, 1)  # NOT 2
+        # remove it
+        pyback.unsubscribe('testchannel', callback)
+        pyback.publish('testchannel', testval=42)
+        self.assertEqual(self.called, 1)  # NOT 2
 
-	def test_object_callback(self):
-		import pyback
-		class Dummy(object):
-			def __init__(self):
-				self.called = 0
+    def test_object_callback(self):
+        import pyback
+        class Dummy(object):
+            def __init__(self):
+                self.called = 0
 
-			def object_callback(self, evt):
-				self.called += 1
+            def object_callback(self, evt):
+                self.called += 1
 
-		dum = Dummy()
-		pyback.subscribe('testchannel', dum.object_callback)
-		pyback.publish('testchannel', thing='blah')
-		self.assertEqual(dum.called, 1)
-		pyback.unsubscribe('testchannel', dum.object_callback)
+        dum = Dummy()
+        pyback.subscribe('testchannel', dum.object_callback)
+        pyback.publish('testchannel', thing='blah')
+        self.assertEqual(dum.called, 1)
+        pyback.unsubscribe('testchannel', dum.object_callback)
 
-	def test_close_channel(self):
-		import pyback
-		self.called = 0
-		def callback(evt):
-			self.called += 1
+    def test_close_channel(self):
+        import pyback
+        self.called = 0
+        def callback(evt):
+            self.called += 1
 
-		pyback.subscribe('testchannel', callback)
-		pyback.publish('testchannel')
-		self.assertEqual(self.called, 1)
-		pyback.close_channel('testchannel')
-		pyback.publish('testchannel')
-		self.assertEqual(self.called, 1)  # NOT 2
+        pyback.subscribe('testchannel', callback)
+        pyback.publish('testchannel')
+        self.assertEqual(self.called, 1)
+        pyback.close_channel('testchannel')
+        pyback.publish('testchannel')
+        self.assertEqual(self.called, 1)  # NOT 2
 
-	def test_two_publishers_one_subscriber(self):
-		import pyback
+    def test_two_publishers_one_subscriber(self):
+        import pyback
 
-		self.called = 0
-		def callback(evt):
-			self.called += 1
+        self.called = 0
+        def callback(evt):
+            self.called += 1
 
-		pyback.subscribe('testchannel', callback)
-		pyback.publish('testchannel')
-		self.assertEqual(self.called, 1)
-		pyback.publish('testchannel')
-		self.assertEqual(self.called, 2)
-		pyback.unsubscribe('testchannel', callback)
+        pyback.subscribe('testchannel', callback)
+        pyback.publish('testchannel')
+        self.assertEqual(self.called, 1)
+        pyback.publish('testchannel')
+        self.assertEqual(self.called, 2)
+        pyback.unsubscribe('testchannel', callback)
 
-	def test_one_publisher_two_subscribers(self):
-		import pyback
+    def test_one_publisher_two_subscribers(self):
+        import pyback
 
-		self.called1 = 0
-		self.called2 = 0
+        self.called1 = 0
+        self.called2 = 0
 
-		def callback1(evt):
-			self.called1 += 1
-		def callback2(evt):
-			self.called2 += 1
+        def callback1(evt):
+            self.called1 += 1
+        def callback2(evt):
+            self.called2 += 1
 
-		pyback.subscribe('testchannel', callback1)
-		pyback.subscribe('testchannel', callback2)
+        pyback.subscribe('testchannel', callback1)
+        pyback.subscribe('testchannel', callback2)
 
-		pyback.publish('testchannel')
+        pyback.publish('testchannel')
 
-		self.assertEqual(self.called1, 1)
-		self.assertEqual(self.called2, 1)
+        self.assertEqual(self.called1, 1)
+        self.assertEqual(self.called2, 1)
 
-		pyback.unsubscribe('testchannel', callback2)
+        pyback.unsubscribe('testchannel', callback2)
 
-		pyback.publish('testchannel')
+        pyback.publish('testchannel')
 
-		self.assertEqual(self.called1, 2)
-		self.assertEqual(self.called2, 1)
+        self.assertEqual(self.called1, 2)
+        self.assertEqual(self.called2, 1)
